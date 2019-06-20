b74c7767e206c176fb3cf001ddd211ee9aaa982a
Shen Suhao <vagrant@homestead>
2019-06-20 02:00:15 +0000

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
